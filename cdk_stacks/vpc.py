307af434ca8d7562da527ef313b0056e33b1ed7b
ksmin23 <sonagy23@gmail.com>
2023-02-03 14:09:27 +0900

    create a new VPC instead of using an existing one

--- cdk_stacks/vpc.py
+++ cdk_stacks/vpc.py
@@ -19,18 +19,20 @@ class VpcStack(Stack):
     # for example,
     # cdk -c vpc_name=your-existing-vpc syth
     #
-    vpc_name = self.node.try_get_context('vpc_name')
-    self.vpc = aws_ec2.Vpc.from_lookup(self, 'ExistingVPC',
-      # is_default=True,
-      vpc_name=vpc_name
-    )
-
-    # self.vpc = aws_ec2.Vpc(self, 'DMSAuroraMysqlToS3VPC',
-    #   max_azs=3,
-    #   gateway_endpoints={
-    #     "S3": aws_ec2.GatewayVpcEndpointOptions(
-    #       service=aws_ec2.GatewayVpcEndpointAwsService.S3
-    #     )
-    #   }
+    # vpc_name = self.node.try_get_context('vpc_name')
+    # self.vpc = aws_ec2.Vpc.from_lookup(self, 'ExistingVPC',
+    #   is_default=True,
+    #   vpc_name=vpc_name
     # )
+
+    #XXX: To use more than 2 AZs, be sure to specify the account and region on your stack.
+    #XXX: https://docs.aws.amazon.com/cdk/api/latest/python/aws_cdk.aws_ec2/Vpc.html
+    self.vpc = aws_ec2.Vpc(self, 'DMSAuroraMysqlToS3VPC',
+      max_azs=3,
+      gateway_endpoints={
+        "S3": aws_ec2.GatewayVpcEndpointOptions(
+          service=aws_ec2.GatewayVpcEndpointAwsService.S3
+        )
+      }
+    )
 
