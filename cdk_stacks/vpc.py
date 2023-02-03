c71070c19748066be1756fcbaf1c83425a3439c9
ksmin23 <sonagy23@gmail.com>
2023-02-03 17:01:37 +0900

    create a new VPC with public and private subnets

--- cdk_stacks/vpc.py
+++ cdk_stacks/vpc.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import os
+import aws_cdk as cdk
 
 from aws_cdk import (
   Stack,
@@ -28,7 +28,24 @@ class VpcStack(Stack):
     #XXX: To use more than 2 AZs, be sure to specify the account and region on your stack.
     #XXX: https://docs.aws.amazon.com/cdk/api/latest/python/aws_cdk.aws_ec2/Vpc.html
     self.vpc = aws_ec2.Vpc(self, 'DMSAuroraMysqlToS3VPC',
+      ip_addresses=aws_ec2.IpAddresses.cidr("10.0.0.0/21"),
       max_azs=3,
+
+      # 'subnetConfiguration' specifies the "subnet groups" to create.
+      # Every subnet group will have a subnet for each AZ, so this
+      # configuration will create `2 groups × 3 AZs = 6` subnets.
+      subnet_configuration=[
+        {
+          "cidrMask": 24,
+          "name": "Public",
+          "subnetType": aws_ec2.SubnetType.PUBLIC,
+        },
+        {
+          "cidrMask": 24,
+          "name": "Private",
+          "subnetType": aws_ec2.SubnetType.PRIVATE_WITH_EGRESS
+        }
+      ],
       gateway_endpoints={
         "S3": aws_ec2.GatewayVpcEndpointOptions(
           service=aws_ec2.GatewayVpcEndpointAwsService.S3
@@ -36,3 +53,5 @@ class VpcStack(Stack):
       }
     )
 
+    cdk.CfnOutput(self, '{}_VPCID'.format(self.stack_name), value=self.vpc.vpc_id,
+      export_name='VPCID')
