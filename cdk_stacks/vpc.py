4bf39f5e7cec4ac9d4b4600035a050f41a1bd083
ksmin23 <sonagy23@gmail.com>
2023-03-09 16:03:06 +0900

    update VPC default ip ranges

--- cdk_stacks/vpc.py
+++ cdk_stacks/vpc.py
@@ -28,7 +28,7 @@ class VpcStack(Stack):
     #XXX: To use more than 2 AZs, be sure to specify the account and region on your stack.
     #XXX: https://docs.aws.amazon.com/cdk/api/latest/python/aws_cdk.aws_ec2/Vpc.html
     self.vpc = aws_ec2.Vpc(self, 'DMSAuroraMysqlToS3VPC',
-      ip_addresses=aws_ec2.IpAddresses.cidr("10.0.0.0/21"),
+      ip_addresses=aws_ec2.IpAddresses.cidr("10.0.0.0/16"),
       max_azs=3,
 
       # 'subnetConfiguration' specifies the "subnet groups" to create.
